8ab3bcffa60e17b52b9a561ea6885d7da51985ac
Trevor <trevor@trevorjackson.ca>
2016-01-07 20:04:07 -0800

    Moved some code into its own function

--- client.go
+++ client.go
@@ -12,14 +12,14 @@ $ go run client.go 127.0.0.1:2020 198.162.52.206:1999
 package main
 
 import (
+	"crypto/md5"
+	"encoding/binary"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"net"
-	"encoding/binary"
 	"os"
 	"strconv"
-	"crypto/md5"
-	"encoding/json"
-	"encoding/hex"
 )
 
 /////////// Msgs used by both auth and fortune servers:
@@ -33,7 +33,7 @@ type ErrMessage struct {
 
 // Message containing a nonce from auth-server.
 type NonceMessage struct {
-	Nonce int64		`json:"Nonce"`
+	Nonce int64 `json:"Nonce"`
 }
 
 // Message containing an MD5 hash from client to auth-server.
@@ -61,84 +61,96 @@ type FortuneMessage struct {
 
 // Open a UDP connection to a given ip address and port
 // Will Exit the program with an error code of 1 if connection fails
-func openConnection(localAddr, remoteAddr string) *net.UDPConn{
+func openConnection(localAddr, remoteAddr string) *net.UDPConn {
 
 	laddr, _ := net.ResolveUDPAddr("udp", localAddr)
 	raddr, _ := net.ResolveUDPAddr("udp", remoteAddr)
-	
+
 	// Open connection to aServer
 	conn, err := net.DialUDP("udp", laddr, raddr)
-	
+
 	// Check if connection was successful
 	if err != nil {
 		fmt.Println("Something has gone wrong in the initial connection\n")
-		os.Exit(1);
+		os.Exit(1)
 	}
-	
+
 	return conn
 
 }
 
 // Send a String using an existing connection to a Server
-func sendString(conn *net.UDPConn, message string){
-	
+func sendString(conn *net.UDPConn, message string) {
+
 	// Send a message to a Server
-	
+
 	buffer := []byte(message)
 	conn.Write(buffer)
 }
 
 // Send bytes using an existing connection to a Server
-func sendBytes(conn *net.UDPConn, message []byte){
-	
+func sendBytes(conn *net.UDPConn, message []byte) {
+
 	conn.Write(message)
-	
+
 }
 
 // read a String using an existing connection to a Server
 // If read fails then Exits wit error code 2
 // returns the read message
-func readMessage(conn *net.UDPConn)[]byte{
-	
+func readMessage(conn *net.UDPConn) []byte {
+
 	buffer := make([]byte, 1024)
-	
+
 	conn.ReadFromUDP(buffer)
-	
+
 	return buffer
 }
 
-func parseNonce(segment []byte)NonceMessage {	
+func parseNonce(segment []byte) NonceMessage {
 	var jsonSection []byte = segment[:29]
-	
+
 	nonce := NonceMessage{}
 	err := json.Unmarshal([]byte(jsonSection), &nonce)
-		
-	if err != nil{
+
+	if err != nil {
 		fmt.Printf("Error Json: %s\n", err)
 	}
-	
+
 	return nonce
 }
 
 // Prints the given message on the console
-func printConsole(message string){
+func printConsole(message string) {
 	fmt.Println(message)
 }
 
-func computeMd5(secret string, nounce int64)string{
+func computeMd5(secret string, nounce int64) string {
 	secretNum, _ := strconv.ParseInt(secret, 10, 64)
 	var toHash int64 = secretNum + nounce
-	
+
 	buf := make([]byte, 64)
-	
+
 	num := binary.PutVarint(buf, toHash)
 	hashByte := buf[:num]
-	
+
 	hasher := md5.New()
 	hasher.Write(hashByte)
 	hash := hasher.Sum(nil)
-	
+
 	return hex.EncodeToString(hash)
+}
+
+func createHashMessage(secret string, nounce NonceMessage) []byte {
+	var hashString string = computeMd5(secret, nonce.Nonce)
+
+	hash := &HashMessage{
+		Hash: hashString,
+	}
+
+	packet, _ := json.Marshal(hash)
+
+	return packet
 }
 
 // Main workhorse method.
@@ -148,31 +160,22 @@ func main() {
 	var localAddr string = os.Args[1]
 	var remoteAddr string = os.Args[2]
 	var secret string = os.Args[3]
-	
-	// Open connection to aServer using given local address and port
+
 	var conn *net.UDPConn = openConnection(localAddr, remoteAddr)
-	
-	// Send Random Message to aServer
-	sendString(conn, "Hello aServer")
-	
-	// Get Message from aServer
+
+	sendString(conn, "Arbitrary Payload")
+
 	var message []byte = readMessage(conn)
-	
+
 	var nonce NonceMessage = parseNonce(message)
 
-	var hashString string = computeMd5(secret, nonce.Nonce)
-	
-	hash := &HashMessage{
-		Hash: hashString,
-	}
-	
-	packet, _ := json.Marshal(hash)
+	var packet []byte = createHashMessage()
+
 	sendBytes(conn, packet)
-	
+
 	response := readMessage(conn)
-	
-	
+
 	fmt.Printf("%s\n", response)
-    conn.Close()
-	
+	conn.Close()
+
 }
